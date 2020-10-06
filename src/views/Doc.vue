88171d9ec00724a22038d2226a73391a0e0c9626
yangyang <nathanyang1991@163.com>
2020-10-06 22:14:50 +0800

    初步搭建2个页面HTML结构

---
+++ src/views/Doc.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="doc">
+    <div class="topnav">
+      <div class="logo">draw ui</div>
+    </div>
+    <div class="content">
+      <div class="aside">侧边栏</div>
+      <div class="main">主内容</div>
+    </div>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <div class="topnav">
+      <div class="logo">draw ui</div>
+    </div>
+    <div class="content">
+      <h1 class="title">draw ui</h1>
+      <h2 class="brief">draw ui description</h2>
+      <p class="actions">
+        <a>github</a>
+        <a>doc</a>
+      </p>
+    </div>
+    <div class="footer">
+      <div class="copyright">Copyright</div>
+    </div>
+  </div>
+</template>
