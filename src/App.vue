05422347f418657974dfcb3fa4c434c18bc6bb20
yangyang <nathanyang1991@163.com>
2020-10-06 21:04:16 +0800

    增加 router-link 切换路由

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
   <Test />
+  <hr>
+  <div>
+    <p>导航：点击切换路由</p>
+    <p>
+      <router-link to='/'>test</router-link>
+      <span>&nbsp|&nbsp</span>
+      <router-link to='/demo'>demo</router-link>
+    </p>
+  </div>
   <router-view />
 </template>
 
