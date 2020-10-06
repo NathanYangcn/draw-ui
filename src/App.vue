ccf306c3d9cba94e35ad225ba2c0c00807719e46
yangyang <nathanyang1991@163.com>
2020-10-06 21:20:02 +0800

    整理并删除无用代码

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
-  <Test />
-  <hr>
   <div>
     <p>导航：点击切换路由</p>
     <p>
       <router-link to='/'>test</router-link>
-      <span>&nbsp|&nbsp</span>
+      |
       <router-link to='/demo'>demo</router-link>
     </p>
   </div>
@@ -15,14 +11,7 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Test from './components/Test.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    Test
-  }
+  name: 'App'
 }
 </script>
