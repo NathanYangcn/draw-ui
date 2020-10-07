de0dd60f3699be890f041db0d3fe79d3addeb5ea
yangyang <nathanyang1991@163.com>
2020-10-07 09:54:39 +0800

    修正页面路由、删除无用文件

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div>
     <p>导航：点击切换路由</p>
     <p>
-      <router-link to='/'>test</router-link>
+      <router-link to='/'>Home</router-link>
       |
-      <router-link to='/demo'>demo</router-link>
+      <router-link to='/doc'>Doc</router-link>
     </p>
   </div>
   <router-view />
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import {createWebHashHistory, createRouter} from 'vue-router'
-import Test from './components/Test.vue'
-import Demo from './components/Demo.vue'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 
 const history = createWebHashHistory()
 const router = createRouter({
   history: history,
   routes: [
-    {path: '/', component: Test},
-    {path: '/demo', component: Demo}
+    {path: '/', component: Home},
+    {path: '/doc', component: Doc}
   ]
 })
 
