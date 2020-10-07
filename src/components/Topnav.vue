6b2053115990e3ae17b5a92ced6655c10071dc30
yangyang <nathanyang1991@163.com>
2020-10-07 20:22:14 +0800

    适配 Topnav

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="topnav">
     <div class="topnav-inner">
+      <div class="toggle-aside">三</div>
       <div class="logo">draw ui</div>
       <div class="actions">
         <router-link to='/'>Home</router-link>
@@ -12,6 +13,9 @@
 </template>
 
 <style lang="scss" scoped>
+@import '../styles/media.scss';
+
+// 默认 PC
 .topnav {
   position: fixed;
   top: 0;
@@ -28,6 +32,25 @@
     height: 60px;
     font-weight: bolder;
     font-style: italic;
+  }
+}
+.toggle-aside {
+  display: none;
+  position: absolute;
+  left: 10px;
+  font-style: normal;
+}
+
+// media phone
+@include phone {
+  .topnav-inner {
+    justify-content: center;
+    .actions {
+      display: none;
+    }
+  }
+  .toggle-aside {
+    display: block;
   }
 }
 </style>
