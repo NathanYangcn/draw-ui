34b6ecb295b6fd828f33569714f34b4aa0843d04
yangyang <nathanyang1991@163.com>
2020-10-08 16:47:40 +0800

    引用路由钩子函数 router.afterEach

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,16 @@
 
 <script>
 import { provide, ref } from 'vue'
+import { router } from './routers'
 
 export default {
   name: 'App',
   setup () {
     const asideVisible = ref(false) // ref() 创建一个响应式对象，通过 .value 获取该值
     provide('asideVisible', asideVisible) // provide ≈ vue.set()
+    router.afterEach(() => {
+      console.log('router afterEach')
+    })
   }
 }
 </script>
